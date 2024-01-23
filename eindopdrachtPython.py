39bcece6d98c5e6d90a69f2e86e2d5b8e788dbb4
kacperfl <134588250+kacperfl@users.noreply.github.com>
2024-01-23 15:22:24 +0100

    Add files via upload

---
+++ eindopdrachtPython.py
@@ -0,0 +1,40 @@
+bin_getal_1 = '0110'
+bin_getal_2 = '1001'
+
+
+decimaal_resultaat = int(bin_getal_1, 2) + int(bin_getal_2, 2)
+
+
+bin_resultaat = bin(decimaal_resultaat)[2:]
+
+print("Resultaat in binair:", bin_resultaat)
+print("Resultaat in decimaal:", decimaal_resultaat)
+
+
+bin_getal_1 = '1111'
+bin_getal_2 = '0101'
+
+decimaal_resultaat = int(bin_getal_1, 2) + int(bin_getal_2, 2)
+bin_resultaat = bin(decimaal_resultaat)[2:]
+
+print("Resultaat in binair:", bin_resultaat)
+print("Resultaat in decimaal:", decimaal_resultaat)
+
+bin_getal_1 = '100110'
+bin_getal_2 = '11011'
+
+decimaal_resultaat = int(bin_getal_1, 2) + int(bin_getal_2, 2)
+bin_resultaat = bin(decimaal_resultaat)[2:]
+
+print("Resultaat in binair:", bin_resultaat)
+print("Resultaat in decimaal:", decimaal_resultaat)
+
+
+bin_getal_1 = '11100'
+bin_getal_2 = '1011'
+
+decimaal_resultaat = int(bin_getal_1, 2) + int(bin_getal_2, 2)
+bin_resultaat = bin(decimaal_resultaat)[2:]
+
+print("Resultaat in binair:", bin_resultaat)
+print("Resultaat in decimaal:", decimaal_resultaat)
